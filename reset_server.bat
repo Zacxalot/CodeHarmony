61e484a0deff1fcea213e834831567edaa92cd38
Samuel Gregson <55996456+Zacxalot@users.noreply.github.com>
2022-03-14 13:34:17 +0000

    Changed nginx config to point to "api" through internal bridge

--- reset_server.bat
+++ reset_server.bat
@@ -1,5 +1,5 @@
 docker build -t code_harmony_nginx  .
-docker run --name chn -p 80:80 -p 8080:8080 -d code_harmony_nginx
+@REM docker run --name chn -p 80:80 -p 8080:8080 -d code_harmony_nginx
 PAUSE
 @REM docker container stop chn
 @REM docker container rm chn
